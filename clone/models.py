92b259a2f899e5cda478c9ee62ab25c4597bfeb1
JamesOdera <nyangoma.odera@gmail.com>
2020-05-08 21:22:36 +0300

    create an update delete create nd list view

--- clone/models.py
+++ clone/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User,  on_delete=models.CASCADE)
+    cover = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('index')
--- clone/urls.py
+++ clone/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import PostListView, PostCreateView, PostDetailView, PostUpdateView, PostDeleteView
 
 urlpatterns=[
-    path('',views.index, name = 'index')
+    path('',PostListView.as_view(),name = 'index'),
+    path('post/new/',PostCreateView.as_view(),name = 'post-create'),
+    path('post/<int:pk>/',PostDetailView.as_view(),name = 'post-detail'),
+    path('post/<int:pk>/update/',PostUpdateView.as_view(),name = 'post-update'),
+    path('post/<int:pk>/delete/',PostDeleteView.as_view(),name = 'post-delete'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
